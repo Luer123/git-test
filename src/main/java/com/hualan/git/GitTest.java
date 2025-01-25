99e8340496e9d2d441468db23da52ae38b18dcee
Luwei <1979975909@qq.com>
2025-01-25 11:02:57 +0800

    second

--- src/main/java/com/hualan/git/GitTest.java
+++ src/main/java/com/hualan/git/GitTest.java
@@ -3,5 +3,6 @@ package com.hualan.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello github");
+        System.out.println("hello github2");
     }
 }
